6f2c21f44572298f3fabb2df7d4923dc2bc76af9
Director of Analytics <=>
2013-01-25 17:12:25 -0500

    adding command line parsing

--- bin/scatter.py
+++ bin/scatter.py
@@ -1,4 +1,6 @@
 import csv
+import optparse
+
 
 def drange(start, stop, step=1.0):
     "generate between 2 numbers w/ optional step"
@@ -20,44 +22,60 @@ def get_scale(series, is_y=False, steps=20):
         scaled_series.reverse()
     return scaled_series
 
+def plot_scatter(f, xs, ys, size, pch):
+    if f:
+        data = [tuple(map(float, line)) for line in csv.reader(open(f))]
+        xs = [i[0] for i in data]
+        ys = [i[1] for i in data]
+    else:
+        xs = [float(row.strip()) for row in open(xs)]
+        ys = [float(row.strip()) for row in open(ys)]
+    
+    plotted = set()
 
-f = "test.txt"
-f = "bigtest.txt"
-f = "coords.txt"
-f = "texas.txt"
+    print "-"*(2*len(get_scale(xs, False, size))+2)
+    for y in get_scale(ys, True, size):
+        print "|",
+        for x in get_scale(xs, False, size):
+            point = " "
+            for (i, (xp, yp)) in enumerate(zip(xs, ys)):
+                if xp <= x and yp >= y and (xp, yp) not in plotted:
+                    point = pch
+                    #point = str(i) 
+                    plotted.add((xp, yp))
+            if x==0 and y==0:
+                point = "o"
+            elif x==0:
+                point = "|"
+            elif y==0:
+                point = "-"
+            print point,
+        print "|"
+    print "-"*(2*len(get_scale(xs, False, size))+2)
 
-if f:
-    data = [tuple(map(float, line)) for line in csv.reader(open(f))]
-else:
-    xs = xs
-    ys = ys
-xs = [i[0] for i in data]
-ys = [i[1] for i in data]
 
-size = 20
+if __name__=="__main__":
 
-plotted = set()
+    parser = optparse.OptionParser()
+    parser.add_option('-f', '--file', help='a csv w/ x and y coordinates',
+                      default=None, dest='f')
+    parser.add_option('-x', help='x coordinates',
+                      default=None, dest='x')
+    parser.add_option('-y', help='y coordinates',
+                      default=None, dest='y')
+    parser.add_option('-s', '--size',help='y coordinates',
+                      default=20, dest='size', type='int')
+    parser.add_option('-p', '--pch',help='shape of point',
+                      default="x", dest='pch') 
 
-print "-"*(2*len(get_scale(xs, False, size))+2)
-for y in get_scale(ys, True, size):
-    print "|",
-    for x in get_scale(xs, False, size):
-        point = " "
-        for (i, (xp, yp)) in enumerate(zip(xs, ys)):
-            if xp <= x and yp >= y and (xp, yp) not in plotted:
-                point = "x"
-                #point = str(i) 
-                plotted.add((xp, yp))
-        if x==0 and y==0:
-            point = "o"
-        elif x==0:
-            point = "|"
-        elif y==0:
-            point = "-"
-        print point,
-    print "|"
-print "-"*(2*len(get_scale(xs, False, size))+2)
-                    
+
+    (opts, args) = parser.parse_args()
+    
+    plot_scatter(opts.f, opts.x, opts.y, opts.size, opts.pch)
+
+#plot_scatter("test.txt")
+#plot_scatter("bigtest.txt")
+#plot_scatter("texas.txt")
 
 
 

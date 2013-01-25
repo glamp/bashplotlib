011a6cbba942db5339c053d664934793744bd5c7
Director of Analytics <=>
2013-01-25 14:36:43 -0500

    updated w/ new python histogram plotter

---
+++ bin/hist.py
@@ -0,0 +1,106 @@
+import math
+from collections import Counter
+import optparse
+
+
+def drange(start, stop, step=1.0):
+    "generate between 2 numbers w/ optional step"
+    r = start
+    while r < stop:
+        yield r
+        r += step
+
+def calc_bins(n, min_val, max_val, h=None):
+    "calculate number of bins for the histogram"
+    if not h:
+        h = math.log(n + 1, 2) 
+    bin_width = (max_val - min_val) / h
+    for b in drange(min_val, max_val, bin_width):
+        yield b
+
+def plot_hist(f, height=20, bincount=None):
+    "plot a histogram given a file of numbers"
+    #first apss
+    min_val, max_val = None, None
+    n = 0.
+    for number in open(f):
+        n += 1
+        number = float(number)
+
+        if not min_val or number < min_val:
+            min_val = number
+        if not max_val or number > max_val:
+            max_val = number
+
+    bins = list(calc_bins(n, min_val, max_val, bincount))
+    hist = Counter()
+    for number in open(f):
+        number = float(number)
+        for i, b in enumerate(bins):
+            if number < b:
+                hist[i-1] += 1
+             #   print "breaking"
+                break
+
+
+
+    min_y, max_y = min(hist.values()), max(hist.values())
+
+    ys = list(drange(min_y, max_y, (max_y-min_y)/height))
+    ys.reverse()
+
+    nlen = max(len(str(min_y)), len(str(max_y))) + 1
+
+
+    for y in ys:
+        ylab = str(y)
+        ylab += " "*(nlen - len(ylab)) + "|"
+
+        print ylab,
+
+        for i in range(len(hist)):
+            if y < hist[i]:
+                print "o",
+            else:
+                print " ",
+        print
+    xs = hist.keys() * 2
+
+    print " "*(nlen+1) + "-"*len(xs)
+
+    for i in range(0, nlen):
+        print " "*(nlen+1),
+        for x in range(0, len(hist)):
+            n = str(bins[x])
+            if x%2==0:
+                print " ",
+            elif i < len(n):
+                print n[i],
+        print
+
+    summary = "Summary\n--------\nMax: %s\nMin:%s" % (min_val, max_val)
+    print summary
+
+
+if __name__=="__main__":
+
+    parser = optparse.OptionParser()
+    parser.add_option('-f', '--file', help='a file containing a column of numbers',
+                      default=None, dest='f')
+    parser.add_option('-b', '--bins', help='number of bins in the histogram',
+                      default=None, dest='b')
+    parser.add_option('-s', help='height of the histogram (in lines)',
+                      default=20, dest='h')
+
+    (opts, args) = parser.parse_args()
+    
+    if opts.f is None:
+        opts.f = args[0]
+    
+    plot_hist(opts.f, opts.h, opts.b)
+
+
+
+
+
+

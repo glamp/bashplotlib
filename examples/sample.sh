2d20ecbd870435bb25dbb3a828c8c0d9391f1a9b
Director of Analytics <=>
2013-01-30 15:19:02 -0500

    changed filenames in bpl

--- examples/sample.sh
+++ examples/sample.sh
@@ -1,24 +1,24 @@
 #plotting coordinates
-../bin/scatter.py -f ../data/texas.txt
+../bin/scatter -f ../data/texas.txt
 
 #with x and y coords
-../bin/scatter.py -x ../data/x_test.txt -y ../data/y_test.txt
+../bin/scatter -x ../data/x_test.txt -y ../data/y_test.txt
 
 #plotting a histogram
-../bin/hist.py -f ../data/exp.txt
+../bin/hist -f ../data/exp.txt
 
 #with colors
-../bin/hist.py -f ../data/exp.txt -c blue
+../bin/hist -f ../data/exp.txt -c blue
 
 #changing the shape of the point
-../bin/hist.py -f ../data/exp.txt -p .
+../bin/hist -f ../data/exp.txt -p .
 
 #using stdin
-curl https://dl.dropbox.com/u/49171662/example.txt | ../bin/hist.py
+curl https://dl.dropbox.com/u/49171662/example.txt | ../bin/hist
 
 #getting data from a webpage
 curl http://www.baseball-reference.com/ \
 | grep  -o -E "[$]([0-9]+)" | grep -o -E "[0-9]+" \
-| ../bin/hist.py -b 20 -t "Baseball Payrolls" --height 20 --pch "*"
+| ../bin/hist -b 20 -t "Baseball Payrolls" --height 20 --pch "*"
 
 

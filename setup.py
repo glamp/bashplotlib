b8a06342fba89ca85933b0d38d01205cb52003c2
Greg Lamp <lamp.greg@gmail.com>
2013-01-30 22:41:07 -0500

    updated description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Greg Lamp",
     author_email="lamp.greg@gmail.com",
     url="https://github.com/glamp/bashplotlib",
-    license=open("LICENSE.txt"),
+    license=open("LICENSE.txt").read(),
     packages=find_packages(),
     description="plotting from the command line",
     long_description=open("README.txt").read(),

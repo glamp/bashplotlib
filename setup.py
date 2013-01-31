16fb3d06b3e573d545a7c4ea4422a74bb9b5f34b
Greg Lamp <lamp.greg@gmail.com>
2013-01-30 22:40:09 -0500

    updated description

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages
 
 setup(
     name="bashplotlib",
-    version="0.3.2",
+    version="0.3.3",
     author="Greg Lamp",
     author_email="lamp.greg@gmail.com",
     url="https://github.com/glamp/bashplotlib",
-    license="LICENSE.txt",
+    license=open("LICENSE.txt"),
     packages=find_packages(),
     description="plotting from the command line",
-    long_description="README.txt",
+    long_description=open("README.txt").read(),
 )
 

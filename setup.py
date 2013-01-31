566535c7d0860171c0f5fd43e6b83fcb9b964cb8
Greg Lamp <lamp.greg@gmail.com>
2013-01-30 22:36:27 -0500

    removed license

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="bashplotlib",
-    version="0.3.0",
+    version="0.3.1",
     author="Greg Lamp",
     author_email="lamp.greg@gmail.com",
     url="https://github.com/glamp/bashplotlib",

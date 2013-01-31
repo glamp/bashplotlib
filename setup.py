4aefc7eef83c96ab7adeba704f80f45a9b52da58
Greg Lamp <lamp.greg@gmail.com>
2013-01-30 22:36:46 -0500

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="bashplotlib",
-    version="0.3.1",
+    version="0.3.2",
     author="Greg Lamp",
     author_email="lamp.greg@gmail.com",
     url="https://github.com/glamp/bashplotlib",

877f667d0256d7ae9eabeed37939214234b60eda
Chris Seymour <chris.j.seymour@hotmail.com>
2014-05-18 13:14:42 +0100

    Dropbox files have moved so added -L to follow redirect. Last two example are broken so commented out for now

--- examples/sample.sh
+++ examples/sample.sh
@@ -1,25 +1,29 @@
-echo plotting coordinates
+#!/usr/bin/env bash
 
+if [ ! -d data ]; then 
+    echo 'downloading data'
+    ./downloaddata.sh
+fi
+
+echo 'plotting coordinates'
 scatter --file ./data/texas.txt
 
-echo with x and y coords
+echo 'with x and y coords'
 scatter -x ./data/x_test.txt -y ./data/y_test.txt
 
-echo plotting a histogram
+echo 'plotting a histogram'
 hist --file ./data/exp.txt
 
-echo with colors
+echo 'with colors'
 hist --file ./data/exp.txt --colour blue
 
-echo changing the shape of the point
+echo 'changing the shape of the point'
 hist --file ./data/exp.txt --pch .
 
-echo using stdin
-curl https://dl.dropbox.com/u/49171662/example.txt | hist
+#echo 'using stdin'
+#curl -sL https://dl.dropbox.com/u/49171662/example.txt | hist
 
-echo getting data from a webpage
-curl http://www.baseball-reference.com/ \
-| grep  -o -E "[$]([0-9]+)" | grep -o -E "[0-9]+" \
-| hist -b 20 -t "Baseball Payrolls" --height 20 --pch "*"
-
-
+#echo 'getting data from a webpage'
+#curl -s 'http://www.baseball-reference.com' | 
+#grep  -o -E '[$][0-9]+' | grep -o -E '[0-9]+' |
+#hist -b 20 -t 'Baseball Payrolls' --height 20 --pch '*'

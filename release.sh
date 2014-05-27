bba921854431e5f8f67ae968d9d86b2b9a4d59cd
Greg Lamp <lamp.greg@gmail.com>
2014-05-27 12:07:23 -0400

    fixing up pypi

---
+++ release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pandoc -f markdown -t rst README.md > README.rst
+python setup.py install sdist $1
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="bashplotlib",
-    version="0.5.3",
+    version="0.6.1",
     author="Greg Lamp",
     author_email="lamp.greg@gmail.com",
     url="https://github.com/glamp/bashplotlib",
-    license=open("LICENSE.txt").read(),
+    license="BSD",
     packages=find_packages(),
     description="plotting in the terminal",
-    long_description=open("README.md").read(),
+    long_description=open("README.rst").read(),
     entry_points = {
         'console_scripts': [
             'hist=bashplotlib.histogram:main',
